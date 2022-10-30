003d3de678bffd35023b63014ca16c394f0f4c5d
Yachef <yachef.h@gmail.com>
2022-10-30 02:03:52 +0200

    Apply spring-boot-tomcat module

---
+++ src/test/java/com/mycompany/myapp/technical/infrastructure/primary/exception/GlobalExceptionHandlerTestController.java
@@ -0,0 +1,56 @@
+package com.mycompany.myapp.technical.infrastructure.primary.exception;
+
+import org.springframework.http.converter.HttpMessageConversionException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@RestController
+@RequestMapping("/api/exception-translator-test")
+public class GlobalExceptionHandlerTestController {
+
+  @PostMapping("/method-argument")
+  public void methodArgument(@Valid @RequestBody TestDTO testDTO) {}
+
+  @GetMapping("/missing-servlet-request-part")
+  public void missingServletRequestPartException(@RequestPart String part) {}
+
+  @GetMapping("/missing-servlet-request-parameter")
+  public void missingServletRequestParameterException(@RequestParam String param) {}
+
+  @GetMapping("/internal-server-error")
+  public void internalServerError() {
+    throw new RuntimeException();
+  }
+
+  @GetMapping("/null-pointer-exception")
+  public void nullPointerException() {
+    throw new NullPointerException("java.lang.NullPointerException");
+  }
+
+  @GetMapping("/http-message-conversion-exception")
+  public void httpMessageConversionException() {
+    throw new HttpMessageConversionException("beer");
+  }
+
+  public static class TestDTO {
+
+    @NotNull
+    private String test;
+
+    public String getTest() {
+      return test;
+    }
+
+    public void setTest(String test) {
+      this.test = test;
+    }
+  }
+}
